33edf8d21aa91abd1f0d0cfada3ec1e9b4fce92c
cristian lopez <cristianlopezvalpo@gmail.com>
2021-04-18 22:23:54 +0000

    modulo para pos screen

---
+++ pos_vitrina_kanban/controllers/controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+# class PosVitrinaKanban(http.Controller):
+#     @http.route('/pos_vitrina_kanban/pos_vitrina_kanban/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/pos_vitrina_kanban/pos_vitrina_kanban/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('pos_vitrina_kanban.listing', {
+#             'root': '/pos_vitrina_kanban/pos_vitrina_kanban',
+#             'objects': http.request.env['pos_vitrina_kanban.pos_vitrina_kanban'].search([]),
+#         })
+
+#     @http.route('/pos_vitrina_kanban/pos_vitrina_kanban/objects/<model("pos_vitrina_kanban.pos_vitrina_kanban"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('pos_vitrina_kanban.object', {
+#             'object': obj
+#         })
